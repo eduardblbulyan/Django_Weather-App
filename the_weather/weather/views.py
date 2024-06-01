901486eb637cefc6e3834c5395168afefba18b24
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-06-01 17:31:19 +0400

    Adding urls and view

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def index(request):
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=27f746592bf575aef976b1677b2f7e5d'
+    city = 'Yerevan'
+    r = requests.get(url.format(city)).json()
+    city_data = {
+        'city': city,
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon']
+    }
+    context = {
+        'city_data': city_data
+    }
+
+    return render(request, 'weather/weather.html',context=context)
