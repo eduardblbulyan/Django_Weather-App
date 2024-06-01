334dbd5693a280e7b780a87bf962f4d7fcc83978
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-06-01 17:53:45 +0400

    Multiple cities view

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=27f746592bf575aef976b1677b2f7e5d'
-    city = 'Yerevan'
-    r = requests.get(url.format(city)).json()
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon']
-    }
-    context = {
-        'city_weather': city_weather
-    }
+    # city = 'Yerevan'
 
-    return render(request, 'weather/weather.html',context=context)
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon']
+        }
+        weather_data.append(city_weather)
+
+    print(weather_data)
+    context = {
+        'weather_data': weather_data
+    }
+    return render(request, 'weather/weather.html', context=context)
