d033005f9ed1af44ba386c57ef2ccab6cf99a7a8
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-06-01 18:08:14 +0400

    Adding multiple cities

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
 
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=27f746592bf575aef976b1677b2f7e5d'
-    # city = 'Yerevan'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
 
     cities = City.objects.all()
 
@@ -22,6 +28,7 @@ def index(request):
 
     print(weather_data)
     context = {
-        'weather_data': weather_data
+        'weather_data': weather_data,
+        'form': form
     }
     return render(request, 'weather/weather.html', context=context)
