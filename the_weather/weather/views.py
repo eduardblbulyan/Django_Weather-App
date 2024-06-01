a985677b3be386ec40a1f7620d388dbee66fde2d
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-06-01 17:37:53 +0400

    Set up for one city

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -5,14 +5,14 @@ def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=27f746592bf575aef976b1677b2f7e5d'
     city = 'Yerevan'
     r = requests.get(url.format(city)).json()
-    city_data = {
+    city_weather = {
         'city': city,
         'temperature': r['main']['temp'],
         'description': r['weather'][0]['description'],
         'icon': r['weather'][0]['icon']
     }
     context = {
-        'city_data': city_data
+        'city_weather': city_weather
     }
 
     return render(request, 'weather/weather.html',context=context)
